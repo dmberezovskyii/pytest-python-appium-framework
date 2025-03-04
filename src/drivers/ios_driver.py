9a5052752f197069aacef9d66e4bc4563e4c5367
dmy.berezovskyi <dmy.berezovskyi@foodtech.team>
2025-03-05 00:07:57 +0200

    fix

--- src/drivers/ios_driver.py
+++ src/drivers/ios_driver.py
@@ -6,4 +6,4 @@ from config import settings
 class iOSDriver:
     @staticmethod
     def get_driver(platform: str):
-        return webdriver.Remote(settings.APPIUM_SERVER, caps)
+        return settings.iOS.to_dict()

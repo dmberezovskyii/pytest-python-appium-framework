976ee95a9e731ace840cba4745734e8558b6100d
dmy.berezovskyi <dmy.berezovskyi@foodtech.team>
2025-03-08 00:47:16 +0200

    added element_interactor.py

--- src/drivers/event_listener.py
+++ src/drivers/event_listener.py
@@ -8,22 +8,25 @@ logger.setLevel(logging.INFO)
 
 
 class AppEventListener(AbstractEventListener):
-	"""Custom Event Listener for Appium WebDriver."""
-	
-	def before_find(self, by, value, driver):
-		logger.info(f"Looking for element: {by} -> {value}")
-	
-	def after_find(self, by, value, driver):
-		logger.info(f"Found element: {by} -> {value}")
-	
-	def before_click(self, element, driver):
-		logger.info(f"Before clicking: {element}")
-	
-	def after_click(self, element, driver):
-		logger.info(f"Clicked on: {element}")
-	
-	def before_quit(self, driver):
-		logger.info("Driver is about to quit.")
-	
-	def after_quit(self, driver):
-		logger.info("Driver has quit.")
+    """Custom Event Listener for Appium WebDriver."""
+
+    def before_find(self, by, value, driver):
+        logger.info(f"Looking for element: {by} -> {value}")
+
+    def after_find(self, by, value, driver):
+        logger.info(f"Found element: {by} -> {value}")
+
+    def before_click(self, element, driver):
+        logger.info(f"Before clicking: {element}")
+
+    def after_click(self, element, driver):
+        logger.info(f"Clicked on: {element}")
+
+    def before_quit(self, driver):
+        logger.info("Driver is about to quit.")
+
+    def after_quit(self, driver):
+        logger.info("Driver has quit.")
+
+    def on_exception(self, exception, driver) -> None:
+        logger.info(f"On exception")
